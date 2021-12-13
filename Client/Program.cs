e6254366881c4686200ad60a2732f9141faff224
andrea basso <andrea.basso@stud.tecnicosuperiorekennedy.it>
2021-12-13 17:58:52 +0100

    aggiunti primi sensori virtuali
    altitudine e gps (latitudine e longitudine)

--- Client/Program.cs
+++ Client/Program.cs
@@ -12,22 +12,24 @@ namespace Client
 
             // init sensors
             List<SensorInterface> sensors = new List<SensorInterface>();
+
             sensors.Add(new VirtualSpeedSensor());
+            sensors.Add(new VirtualGPSSensor());
+            sensors.Add(new VirtualAltitudeSensor());
 
             // define protocol
-            ProtocolInterface protocol = new Http("http://localhost:8011/drones/123");
+            ProtocolInterface protocol = new Http("http://10.30.134.34:8011/drones/123");
 
             // send data to server
             while (true)
             {
                 foreach (SensorInterface sensor in sensors)
                 {
-                    protocol.Send(sensor.toJson());
+                    //protocol.Send(sensor.toJson());
 
                     Console.WriteLine("Data sent: " + sensor.toJson());
 
                     System.Threading.Thread.Sleep(1000);
-
                 }
 
             }
---
+++ Client/Sensors/VirtualGPSSensor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Sensors
+{
+    class VirtualGPSSensor : SensorInterface, GPSSensorInterface
+    {
+        public string toJson()
+        {
+            return "{\"latitude\": " + GetLatitude() + ", \"longitude\": " + GetLongitude() + "}";
+        }
+
+        public double GetLatitude()
+        {
+            var random = new Random();
+            return Math.Round(random.NextDouble() * (90 - (-90)) + (-90), 8);
+        }
+        public double GetLongitude()
+        {
+            var random = new Random();
+            return Math.Round(random.NextDouble() * (180 - (-180)) + (-180), 8);
+        }
+    }
+}
