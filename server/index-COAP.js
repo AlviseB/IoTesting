b49bf2c37300ad81261980d2277d25081591998a
alvise.battistich <alvise.battistich@stud.tecnicosuperiorekennedy.it>
2022-01-19 17:40:11 +0100

    AMQP

---
+++ server/index-COAP.js
@@ -0,0 +1,97 @@
+const sensorModel = require("./src/api/sensor/sensor.model");
+const droneModel = require("./src/api/drone/drone.model");
+
+let subscriptions = [];
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/HTTPDrone", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const coap = require('coap')
+const bl = require('bl')
+
+coap.createServer((req, res) => {
+    console.log("fuck me")
+    path = req.url.split("/");
+    [_, root, droneID, endpoint] = path;
+    method = req.method
+    if (root == "drones")
+        if (method == "POST") {
+            req.pipe(bl((err, data) => {
+                if (err != null) {
+                    process.exit(1)
+                } else {
+                    const json = JSON.parse(data)
+                    drone = droneModel.create(json)
+                    console.log("created ", drone)
+                }
+            }))
+            res.end()
+        }
+    if (method == "GET" && endpoint == "action") {
+        console.log("sent command to drone:", droneID)
+        res.end('{"command": "hold"}')
+    }
+}).listen(() => console.log("Running")/*{
+    coap
+        .request({
+            pathname: '/Matteo/otherpath',
+            options: {
+                Accept: 'application/json'
+            }
+        })
+        .on('response', (res) => {
+            console.log('response code', res.code)
+            if (res.code !== '2.05') {
+                return process.exit(1)
+            }
+
+            res.pipe(bl((err, data) => {
+                if (err != null) {
+                    process.exit(1)
+                } else {
+                    const json = JSON.parse(data)
+                    console.log(json)
+                    process.exit(0)
+                }
+            }))
+        })
+        .end()
+}*/)
+
+/*
+const root = "iot2021";
+const version = "v1";
+const zone = "thiene";
+
+client.on('connect', function () {
+  let topic = `${root}/${version}/${zone}/+/status/#`;
+  client.subscribe(topic, function (err) {
+    subscriptions.push(topic)
+    console.log("subscribed to #");
+  })
+})
+
+client.on('message', async function (topic, message) {
+  // message is Buffer
+  let [root, version, zone, droneID, status, sensorType] = topic.split("/");
+  console.log(message.toString());
+  data = JSON.parse(message.toString());
+  sensorData = {};
+  sensorData.droneID = droneID;
+  sensorData.timestamp = (new Date()).toUTCString();
+  sensorData.sensorType = sensorType;
+  sensorData.value = data[sensorType];
+  console.log(sensorData);
+  res = await sensorModel.create(sensorData);
+  console.log("Added: ", res);
+})
+
+setInterval(() => {
+  const drone = "dr1_42"
+  client.publish(`${root}/${version}/${zone}/${drone}/command`, '{"command": "none"}')
+
+}, 1000);*/
+
--- server/index.js
+++ server/index.js
@@ -9,57 +9,44 @@ mongoose.connect("mongodb://localhost:27017/HTTPDrone", {
     useUnifiedTopology: true,
 });
 
-const coap = require('coap')
-const bl = require('bl')
+const amqp = require('amqplib/callback_api');
 
-coap.createServer((req, res) => {
-    console.log("fuck me")
-    path = req.url.split("/");
-    [_, root, droneID, endpoint] = path;
-    method = req.method
-    if (root == "drones")
-        if (method == "POST") {
-            req.pipe(bl((err, data) => {
-                if (err != null) {
-                    process.exit(1)
-                } else {
-                    const json = JSON.parse(data)
-                    drone = droneModel.create(json)
-                    console.log("created ", drone)
-                }
-            }))
-            res.end()
-        }
-    if (method == "GET" && endpoint == "action") {
-        console.log("sent command to drone:", droneID)
-        res.end('{"command": "hold"}')
+amqp.connect('amqps://kxbxxcqx:IqK-VIirhbu-Gmil2bED0EsPXUxfeaa2@goose.rmq2.cloudamqp.com/kxbxxcqx', function (error0, connection) {
+    if (error0) {
+        throw error0;
     }
-}).listen(() => console.log("Running")/*{
-    coap
-        .request({
-            pathname: '/Matteo/otherpath',
-            options: {
-                Accept: 'application/json'
-            }
-        })
-        .on('response', (res) => {
-            console.log('response code', res.code)
-            if (res.code !== '2.05') {
-                return process.exit(1)
-            }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
 
-            res.pipe(bl((err, data) => {
-                if (err != null) {
-                    process.exit(1)
-                } else {
-                    const json = JSON.parse(data)
-                    console.log(json)
-                    process.exit(0)
-                }
-            }))
-        })
-        .end()
-}*/)
+        exchange = "AMQP-DRONE";
+        queue = "dr1_42"
+
+        channel.assertExchange(exchange, 'topic')
+        channel.assertQueue(queue);
+        channel.bindQueue(queue, exchange, `${queue}.#`);
+
+        channel.assertQueue("server");
+        channel.assertExchange("AMQP-SERVER", 'direct')
+        channel.bindQueue("server", "AMQP-SERVER");
+
+
+        setInterval(() => {
+            const drone = "dr1_42"
+            channel.publish("AMQP-DRONE", `${drone}.command`, Buffer.from('{"command": "hold"}'))
+        }, 5000);
+
+            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume("server", function (msg) {
+            drone = JSON.parse(msg.content);
+            droneModel.create(drone).then((v) => console.log(v))
+            console.log(" [x] Received %s", drone);
+        }, {
+            noAck: true
+        });
+    });
+});
 
 /*
 const root = "iot2021";
